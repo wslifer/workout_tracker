ef00ad0134dd6b420a3167d31c1714e2cd771fff
Wesley Slifer <wesleyslifer@ymail.com>
2020-07-19 18:40:21 -0500

    starting on api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,8 @@
+// Requiring files/packages
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+// Api Routes
+
+// Exporting for use in other files
+module.exports = router;
