9306d1b92c926b161471124e07ff39ac34ba95f5
Wesley Slifer <wesleyslifer@ymail.com>
2020-07-19 19:17:28 -0500

    updating apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,5 +4,52 @@ const Workout = require("../models/workout");
 
 // Api Routes
 
+// get routes
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// post routes
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// update routes
+router.put("api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdandUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 // Exporting for use in other files
 module.exports = router;
